8df3479ea505716c7b7e9e9606694d60a240c362
JasonWei <9979023+timesons@users.noreply.github.com>
2024-07-30 18:05:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.config={
-    logo: 'https://img2.imgtp.com/2024/03/11/YzU8iDJp.png', // 网站logo
-    title:"AirBus", // 网站标题
+    // logo: 'https://img2.imgtp.com/2024/03/11/YzU8iDJp.png', // 网站logo
+    title:"GJTQ", // 网站标题
     host:"https://login.gjtq.net/", // 后端接口地址
     storeHome: {
         title: '选择最适合你的订阅计划', // 套餐页标题
