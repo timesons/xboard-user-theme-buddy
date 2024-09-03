61f2a58f8da45de9e25fb992269ed5ae8e1c537e
JasonWei <9979023+timesons@users.noreply.github.com>
2024-09-04 03:31:46 +0800

    a
    a

--- assets/Layout-D6r5F39j.js
+++ assets/Layout-D6r5F39j.js
@@ -60798,26 +60798,7 @@ const c$ = "_card_1p3bd_1",
                 splitLine: { show: !1 },
               },
             ],
-            yAxis: [
-              {
-                type: "value",
-                splitLine: { show: !1 },
-                axisLabel: {
-                  show: !0,
-                  formatter: (p) =>
-                    p > 5
-                      ? `${((p - 5) * 1e3).toFixed(0)}GB`
-                      : p > 4
-                      ? `${((p - 4) * 1e3).toFixed(0)}M`
-                      : p > 3
-                      ? `${((p - 3) * 1e3).toFixed(0)}KB`
-                      : `${(p - 2).toFixed(1)}B`,
-                },
-                min: 2.8,
-                max: 5.2,
-                interval: 0.5,
-              },
-            ],
+    
             series: [
               {
                 name: "总消耗流量",
