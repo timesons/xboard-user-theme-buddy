15b44e92bebbb5dfc4718e3bf54a9a7766a67910
JasonWei <9979023+timesons@users.noreply.github.com>
2024-09-05 03:09:48 +0800

    a
    a

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.config={
-    logo: 'https://user.gjtq.net/dog-logo.svg', // 网站logo
+    logo: 'https://user.gjtq.net/woa%402x.png', // 网站logo
     title:"范小强VPN", // 网站标题
     host:"https://airport404.com/", // 后端接口地址
     storeHome: {
