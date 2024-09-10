b9f25fb1dc26400e65bef004333205820d7ab382
JasonWei <9979023+timesons@users.noreply.github.com>
2024-09-11 03:00:24 +0800

    a
    a

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.config={
-    logo: './woa2x.png', // 网站logo
+    logo: './Rainbow.png', // 网站logo
     title:"PassWall.Cloud", // 网站标题
     host:"https://airport404.com/", // 后端接口地址
     storeHome: {
